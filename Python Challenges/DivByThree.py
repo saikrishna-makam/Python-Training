9cb2ba527e5e6957161c7868848946acd911743c
saikrishna.makam@cuelogic.com <saikrisnamakam43770@gmail.com>
2021-05-12 21:26:58 +0530

    Done with the Python Challenges/Assignments(Day 9)

---
+++ Python Challenges/DivByThree.py
@@ -0,0 +1,13 @@
+#Problem Title: Divisible by 3
+
+#Problem:
+
+#Define a function named div_3 that returns True if its single integer parameter is divisible by 3 and False otherwise.
+
+#For example, div_3(6) is True because 6/3 does not leave any remainder. However div_3(5) is False because 5/3 leaves 2 as a 
+#remainder.
+
+#Solution:
+
+def div_3(val):
+    return val % 3 == 0
---
+++ Python Challenges/FlattenList.py
@@ -0,0 +1,17 @@
+#Problem Title: Flatten a list
+
+#Problem: Write a function that takes a list of lists and flattens it into a one-dimensional list.
+
+#Name your function flatten. It should take a single parameter and return a list.
+
+#For example, calling:
+
+#flatten([[1, 2], [3, 4]])
+#Should return the list:
+
+#[1, 2, 3, 4]
+
+#Solution:
+
+def flatten(nestedList):
+    return [val for list in nestedList for val in list]
